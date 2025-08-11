c8204f92a3c9287439acf9c98af95cd591097acc
NikaNishnianidze <nikanishno8@gmail.com>
2025-08-11 18:49:42 +0400

    add react responsive for responsive header design

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,12 @@
+import DesktopHeader from "./Components/DesktopHeader";
 import Header from "./Components/Header";
+import { useMediaQuery } from "react-responsive";
 
 function App() {
+  const isDesktop = useMediaQuery({ minWidth: 1440 });
   return (
     <div className="flex flex-col">
+      {isDesktop && <DesktopHeader />}
       <Header />
     </div>
   );
---
+++ src/Components/DesktopHeader.tsx
@@ -0,0 +1,5 @@
+export default function DesktopHeader() {
+  return (
+    <div className="flex items-center w-full h-[44px] py-[14px] pl-[60px] bg-[#003A46]"></div>
+  );
+}

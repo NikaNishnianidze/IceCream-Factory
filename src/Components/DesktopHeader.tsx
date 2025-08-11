73f02ef60b64522e09f6e4f1c1247702876486fc
NikaNishnianidze <nikanishno8@gmail.com>
2025-08-11 18:53:09 +0400

    finish desktopHeader design

--- src/Components/DesktopHeader.tsx
+++ src/Components/DesktopHeader.tsx
@@ -1,5 +1,12 @@
 export default function DesktopHeader() {
   return (
-    <div className="flex items-center w-full h-[44px] py-[14px] pl-[60px] bg-[#003A46]"></div>
+    <div className="flex items-center w-full h-[44px] py-[14px] pl-[60px] bg-[#003A46]">
+      <div className="buttons w-[1200px] flex items-center justify-between text-[#AAAAAA] text-[14px] font-normal">
+        <p className="cursor-pointer">მენეჯერისთვის</p>
+        <p className="cursor-pointer">ადმინისტრატორისთვის</p>
+        <p className="cursor-pointer">დისტრიბუტორისთვის</p>
+        <p className="cursor-pointer">გაყიდვების გუნდისთვის</p>
+      </div>
+    </div>
   );
 }

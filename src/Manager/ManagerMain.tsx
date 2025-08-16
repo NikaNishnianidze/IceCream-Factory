157d3aeeb8562734f5cf925397229d3f0f8535f4
NikaNishnianidze <nikanishno8@gmail.com>
2025-08-16 14:00:10 +0400

    install recharts for charts design

--- src/Manager/ManagerMain.tsx
+++ src/Manager/ManagerMain.tsx
@@ -1,3 +1,9 @@
 export default function ManagerMain() {
-  return <div className="w-[807px] p-[20px] bg-[#003A46] rounded-[6px]"></div>;
+  return (
+    <div className="w-[807px] p-[20px] bg-[#003A46] rounded-[6px] flex flex-col gap-[25px]">
+      <div className="main-div flex justify-between items-start">
+        <div className="ingredients-chart flex flex-col"></div>
+      </div>
+    </div>
+  );
 }
